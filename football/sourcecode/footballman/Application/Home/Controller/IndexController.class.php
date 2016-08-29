295f6fb4918cbeb36e292a9402cad05145e50f60
chenrongguang <chenrongguang@rongcapital.cn>
2016-08-29 17:20:26 +0800

    发送邮件测试类

--- football/sourcecode/footballman/Application/Home/Controller/IndexController.class.php
+++ football/sourcecode/footballman/Application/Home/Controller/IndexController.class.php
@@ -25,6 +25,7 @@ body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px}
         $this->display();
     }
 
+    //测试DB
     public function testdb()
     {
         $Model = M("test");
@@ -40,4 +41,26 @@ body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px}
         */
     }
 
+    //测试发送邮件
+    /*
+     *最终的测试结果是，在发送给某些邮件的时候会报错，但是有些邮箱又没有问题
+     * 还不知道什么原因，估计和接收方有关系
+     */
+    public  function  testsendmail(){
+         Vendor('SendMail.sendmail');
+         $mail = new \MySendMail();
+         $mail->setServer("smtp.163.com", "ruixuesoft_develop@163.com", "ruixuesoft"); //设置smtp服务器
+         $mail->setFrom("ruixuesoft_develop@163.com"); //设置发件人
+         $mail->setReceiver("chenrongguang@rongcapital.cn"); //设置收件人，多个收件人，调用多次
+         //$mail->setCc(""); //设置抄送，多个抄送，调用多次
+         //$mail->setBcc("XXXXX"); //设置秘密抄送，多个秘密抄送，调用多次
+         $mail->setMailInfo("test", "<b>test by crg </b>"); //设置邮件主题、内容
+         $result= $mail->sendMail(); //发送
+        if($result==true){
+            echo 'success';
+        }
+        else{
+            echo 'fail';
+        }
+    }
 }
